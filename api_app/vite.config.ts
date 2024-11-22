d2faf33b22e1eac575fd47c475a3cadd7547039f
Dorian Badel <dorianbadel@protonmail.com>
2024-11-22 01:11:35 +0100

    sw fixes

--- api_app/vite.config.ts
+++ api_app/vite.config.ts
@@ -1,9 +1,31 @@
-
-// vite.config.js / vite.config.ts
+import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-export default {
+export default defineConfig({
   plugins: [
-    VitePWA()
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}'],
+        cleanupOutdatedCaches: true
+      },
+      manifest: {
+        name: "React_api",
+        short_name: "GHB",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        theme_color: "cyan",
+        background_color: "white",
+        icons: [
+          {
+            src: "/assets/pwa_logo.png",
+            sizes: "144x144",
+            type: "image/png"
+          }
+        ]
+      }
+    })
   ]
-}
+})
