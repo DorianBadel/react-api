6aa84c594cfb354a9a89accc3b87609978d25d3f
Dorian Badel <DorianBadel@protonmail.com>
2024-12-23 23:13:04 +0100

    Formating

--- api_app/public/sw.js
+++ api_app/public/sw.js
@@ -1,56 +1,63 @@
 console.log("Herrooo from service workor");
 
-if (Notification.permission != "granted") {
-  Notification.requestPermission();
-}
 self.addEventListener("fetch", (event) => {
   if (event.request.url.includes("/graphql")) {
     console.log("Intercepted GraphQL request:", event.request.url);
     event.respondWith(
       fetch(event.request).then((response) => {
         const clonedResponse = response.clone();
-        clonedResponse.json().then((data) => {
-          console.log("GraphQL response data:", data);
-          // Check if the response contains the friends array
-          if (data.data && Array.isArray(data.data.friends)) {
-            const birthdays = data.data.friends.map(
-              (friend) => friend.friend.date_of_birth
-            );
-            console.log("Extracted birthdays:", birthdays);
-            if (birthdays && birthdays.length > 0) {
-              const now = new Date();
-              let closestBirthday = null;
-              let birthdayPerson = null;
-              let minDiff = Infinity;
+        clonedResponse.text().then((text) => {
+          try {
+            const data = JSON.parse(text);
+            console.log("GraphQL response data:", data);
+            // Check if the response contains the friends array
+            if (data.data && Array.isArray(data.data.friends)) {
+              const birthdays = data.data.friends.map(
+                (friend) => friend.friend.date_of_birth
+              );
+              console.log("Extracted birthdays:", birthdays);
+              if (birthdays && birthdays.length > 0) {
+                const now = new Date();
+                let closestBirthday = null;
+                let birthdayPerson = null;
+                let minDiff = Infinity;
 
-              birthdays.forEach((birthdayStr) => {
-                const birthday = new Date(birthdayStr);
-                birthday.setFullYear(now.getFullYear());
-                if (birthday < now) {
-                  birthday.setFullYear(now.getFullYear() + 1);
+                birthdays.forEach((birthdayStr) => {
+                  const birthday = new Date(birthdayStr);
+                  birthday.setFullYear(now.getFullYear());
+                  if (birthday < now) {
+                    birthday.setFullYear(now.getFullYear() + 1);
+                  }
+                  const diff = birthday - now;
+                  if (diff < minDiff) {
+                    minDiff = diff;
+                    closestBirthday = birthday;
+                    birthdayPerson =
+                      data.data.friends[birthdays.indexOf(birthdayStr)].friend;
+                  }
+                });
+                console.log("Closest birthday:", closestBirthday);
+
+                if (closestBirthday) {
+                  const oneWeekInMilliseconds = 7 * 24 * 60 * 60 * 1000; // 1 week in milliseconds
+
+                  const birthdayDate = new Date(birthdayPerson.date_of_birth);
+                  const currentDate = new Date();
+                  const timeDiff =
+                    birthdayDate.getTime() - currentDate.getTime();
+
+                  if (timeDiff > 0 && timeDiff <= oneWeekInMilliseconds) {
+                    console.log("Showing notification");
+                    self.registration.showNotification("Upcoming Birthday", {
+                      body: `It is ${birthdayPerson.name}'s birthday in one week`,
+                      icon: "/assets/pwa_logo.png",
+                    });
+                  }
                 }
-                const diff = birthday - now;
-                if (diff < minDiff) {
-                  minDiff = diff;
-                  closestBirthday = birthday;
-                  birthdayPerson =
-                    data.data.friends[birthdays.indexOf(birthdayStr)].friend;
-                }
-              });
-              console.log("Closest birthday:", closestBirthday);
-
-              if (closestBirthday) {
-                const timeUntilNotification = 5 * 60 * 100; // 5 minutes in milliseconds
-
-                setTimeout(() => {
-                  console.log("Showing notification");
-                  self.registration.showNotification("Upcoming Birthday", {
-                    body: `It is ${birthdayPerson.name}'s birthday soon`,
-                    icon: "/assets/pwa_logo.png",
-                  });
-                }, timeUntilNotification);
               }
             }
+          } catch (error) {
+            console.error("Failed to parse JSON:", error);
           }
         });
         return response;
--- api_app/src/Pages/Home/Home.tsx
+++ api_app/src/Pages/Home/Home.tsx
@@ -1,64 +1,13 @@
-//import { useMyQueryQuery } from '../utils/__generated__/graphql'
-import { useMutation, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import MainPage from "../../components/MainPage";
 import FriendsSection from "../../components/FriendsSection";
-import { CREATE_FRIEND, CREATE_USER, ThisUser } from "../../api/queries";
+import { ThisUser } from "../../api/queries";
 import MainBody from "../../components/MainBody";
-import { Button } from "@/components/ui/button"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useState } from "react";
+import { ID } from "@/global/variables";
+import AddBirthday from "@/components/forms/AddBirthday";
 
-const id = 7;
 export const Page = () => {
-  const { loading, data, error } = useQuery(ThisUser(id));
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [birthday, setBirthday] = useState("");
-
-  const [createUser, { loading: createUserLoading, error: createUserError }] = useMutation(CREATE_USER);
-  const [createFriend, { loading: createFriendLoading, error: createFriendError }] = useMutation(CREATE_FRIEND);
-
-  const createLoading = createUserLoading || createFriendLoading;
-  const createError = createUserError || createFriendError;
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const createdUser = await createUser({
-        variables: {
-          name: name,
-          date_of_birth: birthday,
-          UID: id,
-        },
-      });
-      console.log(createdUser);
-
-      await createFriend({
-        variables: {
-          UID: id,
-          FID: createdUser.data.insert_users.returning[0].ID,
-          nickname: nickname ?? null,
-        },
-      });
-      setIsModalOpen(false);
-      // Optionally, you can refetch the user's data to update the UI
-    } catch (error) {
-      console.error("Error creating friend:", error);
-    }
-  };
+  const { loading, data, error } = useQuery(ThisUser(ID));
 
   return (
     <MainBody>
@@ -67,52 +16,12 @@ export const Page = () => {
       ) : !error ? (
         <section>
           <MainPage name={data.users[0].name} />
-          <FriendsSection ID={id} />
+          <FriendsSection ID={ID} />
+          <AddBirthday />
         </section>
       ) : (
         <h1> ERROR - {error.message}</h1>
       )}
-      <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
-      <DialogTrigger asChild>
-      <Button size="icon" className="fixed bottom-4 right-4 rounded-full p-2 text-2xl shadow-lg">+</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Add birthday</DialogTitle>
-          <DialogDescription>
-            Add a new birthday
-          </DialogDescription>
-        </DialogHeader>
-        <form onSubmit={handleSubmit}>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="name" className="text-right">
-                Name
-              </Label>
-              <Input id="name" value={name} onChange={(e) => setName(e.target.value)} className="col-span-3" />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="nickname" className="text-right">
-                Nickname
-              </Label>
-              <Input id="nickname" value={nickname} onChange={(e) => setNickname(e.target.value)} className="col-span-3" />
-            </div>
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="dob" className="text-right">
-                Date of Birth
-              </Label>
-              <Input id="dob" value={birthday} onChange={(e) => setBirthday(e.target.value)} className="col-span-3" />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit" disabled={createLoading}>
-              {createLoading ? "Adding..." : "Add"}
-            </Button>
-          </DialogFooter>
-        </form>
-        {createError && <p className="text-red-500">{createError.message}</p>}
-      </DialogContent>
-    </Dialog>
     </MainBody>
   );
 };
